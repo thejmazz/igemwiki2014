0ba9f7792bb6f56c6c76400848d9830a35ed8f96
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2014-09-17 21:01:42 -0400

    Proper header.php and footer.php; Made some little bootstrap changes to home

---
+++ header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<title>Team:Toronto - 2014.igem.org</title>
+		
+		<!-- Bootstrap -->
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+	</head>
+	<body>
+		
+	
--- home.php
+++ home.php
@@ -1,191 +1,213 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
+<?php
+include ('header.php');
+?>
+<!-- NAVBAR
+================================================== -->
+	<div class="navbar-wrapper">
+		<div class="container">
 
-		<title>Team:Toronto - 2014.igem.org</title>
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
-	</head>
-	<body>
-		<!--main content -->
-		<table width="70%" align="center">
+			<div class="navbar navbar-inverse navbar-static-top" role="navigation">
+				<div class="container">
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a class="navbar-brand" href="#">Project name</a>
+					</div>
+					<div class="navbar-collapse collapse">
+						<ul class="nav navbar-nav">
+							<li class="active">
+								<a href="#">Home</a>
+							</li>
+							<li>
+								<a href="#about">About</a>
+							</li>
+							<li>
+								<a href="#contact">Contact</a>
+							</li>
+							<li class="dropdown">
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
+								<ul class="dropdown-menu" role="menu">
+									<li>
+										<a href="#">Action</a>
+									</li>
+									<li>
+										<a href="#">Another action</a>
+									</li>
+									<li>
+										<a href="#">Something else here</a>
+									</li>
+									<li class="divider"></li>
+									<li class="dropdown-header">
+										Nav header
+									</li>
+									<li>
+										<a href="#">Separated link</a>
+									</li>
+									<li>
+										<a href="#">One more separated link</a>
+									</li>
+								</ul>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
 
-			<!--welcome box -->
-			<tr>
-				<td style="border:1px solid black;" colspan="3" align="center" height="150px" bgColor=#FF404B><h1 >WELCOME TO iGEM 2014! </h1>
+		</div>
+	</div>
+
+	<!-- Carousel
+	================================================== -->
+	<div id="myCarousel" class="carousel slide" data-ride="carousel">
+		<!-- Indicators -->
+		<ol class="carousel-indicators">
+			<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+			<li data-target="#myCarousel" data-slide-to="1"></li>
+			<li data-target="#myCarousel" data-slide-to="2"></li>
+		</ol>
+		<div class="carousel-inner">
+			<div class="item active">
+				<img src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="First slide">
+				<div class="container">
+					<div class="carousel-caption">
+						<h1>Example headline.</h1>
+						<p>
+							Note: If you're viewing this page via a
+							<code>
+								file://
+							</code>
+							URL, the "next" and "previous" Glyphicon buttons on the left and right might not load/display properly due to web browser security rules.
+						</p>
+						<p>
+							<a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a>
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="item">
+				<img src="data:image/gif;base64,R0lGODlhAQABAIAAAGZmZgAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Second slide">
+				<div class="container">
+					<div class="carousel-caption">
+						<h1>Another example headline.</h1>
+						<p>
+							Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+						</p>
+						<p>
+							<a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a>
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="item">
+				<img src="data:image/gif;base64,R0lGODlhAQABAIAAAFVVVQAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Third slide">
+				<div class="container">
+					<div class="carousel-caption">
+						<h1>One more for good measure.</h1>
+						<p>
+							Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+						</p>
+						<p>
+							<a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a>
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+		<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+	</div><!-- /.carousel -->
+
+	<!-- Marketing messaging and featurettes
+	================================================== -->
+	<!-- Wrap the rest of the page in another container to center all the content. -->
+
+	<div class="container marketing">
+
+		<!-- Three columns of text below the carousel -->
+		<div class="row">
+			<div class="col-lg-4">
+				<img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" style="width: 140px; height: 140px;">
+				<h2>Heading</h2>
 				<p>
-					Your team has been approved and you are ready to start the iGEM season!
-					<br>
-					On this page you can document your project, introduce your team members, document your progress
-					<br>
-					and share your iGEM experience with the rest of the world!
+					Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.
 				</p>
-				<br>
-				<p style="color:#E7E7E7">
-					<a href="http://2014.igem.org/wiki/index.php?title=Team:Toronto&action=edit"style="color:#FFFFFF"> Click here  to edit this page!</a>
-				</p></td>
-			</tr>
-
-			<tr>
-				<td colspan="3"  height="5px"></td>
-			</tr>
-			<!-- end welcome box -->
-			<tr>
-				<?php include 'navmenulocal.php'; ?>
-			</tr>
-
-			</tr>
-
-			</td>
-
-			<tr>
-				<td colspan="3"  height="15px"></td>
-			</tr>
-			<tr>
-				<td bgColor="#e7e7e7" colspan="3" height="1px">
-			</tr>
-			<tr>
-				<td colspan="3"  height="5px"></td>
-			</tr>
-
-			<!--requirements section -->
-			<tr>
-				<td colspan="3"><h3> Requirements </h3></td>
-			</tr>
-			<tr>
-				<td width="45%"  valign="top">
 				<p>
-					Please be sure to keep these links, your audience will want to find your:
-				</p><!-- Links to other team pages -->
-				<ul>
-					<li>
-						<a href="http://2014.igem.org/Team:Toronto">Home</a>
-					</li>
-					<li>
-						<a href="http://2014.igem.org/Team:Toronto/Team">Team</a>
-					</li>
-					<li>
-						<a href="http://igem.org/Team.cgi?year=2013&team_name=Toronto">Official Team Profile</a>
-					</li>
-					<li>
-						<a href="http://2014.igem.org/Team:Toronto/Project">Project</a>
-					</li>
-					<li>
-						<a href="http://2014.igem.org/Team:Toronto/Parts">Parts</a>
-					</li>
-					<li>
-						<a href="http://2014.igem.org/Team:Toronto/Modeling">Modeling</a>
-					</li>
-					<li>
-						<a href="http://2014.igem.org/Team:Toronto/Notebook">Notebook</a>
-					</li>
-					<li>
-						<a href="http://2014.igem.org/Team:Toronto/Safety">Safety</a>
-					</li>
-					<li>
-						<a href="http://2014.igem.org/Team:Toronto/Attributions">Attributions</a>
-					</li>
-
-				</ul></td>
-
-				<td ></td>
-				<td width="45%">
-				<p>
-					There are a few wiki requirements teams must follow:
+					<a class="btn btn-default" href="#" role="button">View details &raquo;</a>
 				</p>
-				<ul>
-					<li>
-						All pages, images and files must be hosted on the <a href ="http://2014.igem.org/Special:Upload"> 2014.igem.org server</a>.
-					</li>
-					<li>
-						All pages must be created under the team’s name space.
-					</li>
-					<li>
-						As part of your documentation, keep the links from the menu to the left.
-					</li>
-					<li>
-						Do not use flash in wiki code.
-					</li>
-					<li>
-						The <a href="http://igem.org/wiki/images/6/60/Igemlogo_300px.png"> iGEM logo </a> should be placed on the upper part of every page and should link to <a href="http://2014.igem.org/Main_Page">2014.igem.org</a>.
-					</li>
-				</ul>
+			</div><!-- /.col-lg-4 -->
+			<div class="col-lg-4">
+				<img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" style="width: 140px; height: 140px;">
+				<h2>Heading</h2>
 				<p>
-					Visit the <a href="http://2014.igem.org/Wiki_How-To"> Wiki How To page </a> for a complete list of requirements, tips and other useful information.
-				</p></td>
-			</tr>
-
-			<tr>
-				<td colspan="3"  height="15px"></td>
-			</tr>
-			<tr>
-				<td bgColor="#e7e7e7" colspan="3" height="1">
-			</tr>
-
-			<!--tips  -->
-			<tr>
-				<td colspan="3" ><h3> Tips </h3></td>
-			</tr>
-
-			<tr>
-				<td width="45%" valign="top">
-				<p>
-					We are currently working on providing teams with some easy to use design templates.
-					<br>
-					In the meantime you can also view other team wikis for inspiration! Here are some very good examples
+					Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.
 				</p>
-				<ul>
-					<li>
-						<a href="http://2013.igem.org/Team:SDU-Denmark/"> 2013 SDU Denmark </a>
-					</li>
-					<li>
-						<a href="http://2013.igem.org/Team:SYSU-China">2013 SYSU China</a>
-					</li>
-					<li>
-						<a href="http://2013.igem.org/Team:Shenzhen_BGIC_ATCG"> 2013 Shenxhen BGIG ATCG </a>
-					</li>
-					<li>
-						<a href="http://2013.igem.org/Team:Colombia_Uniandes">2013 Colombia Unianades </a>
-					</li>
-					<li>
-						<a href="http://2013.igem.org/Team:Lethbridge">2013 Lethbridge</a>
-					</li>
-				</ul>
 				<p>
-					For a full wiki list, you can visit <a href="http://igem.org/Team_Wikis?year=2013">iGEM 2013 web sites </a> and <a href="http://igem.org/Team_Wikis?year=2012">iGEM 2012 web sites</a> lists.
-				</p></td>
-
-				<td></td>
-				<td width="45%">
-				<p>
-					This wiki will be your team’s first interaction with the rest of the world, so here are a few tips to help you get started:
+					<a class="btn btn-default" href="#" role="button">View details &raquo;</a>
 				</p>
-				<ul>
-					<li>
-						State your accomplishments! Tell people what you have achieved from the start.
-					</li>
-					<li>
-						Be clear about what you are doing and what you plan to do.
-					</li>
-					<li>
-						You have a global audience! Consider the different backgrounds that your users come from.
-					</li>
-					<li>
-						Make sure information is easy to find; nothing should be more than 3 clicks away.
-					</li>
-					<li>
-						Avoid using very small fonts and low contrast colors; information should be easy to read.
-					</li>
-					<li>
-						Start documenting your project as early as possible; don’t leave anything to the last minute before the Wiki Freeze. For a complete list of deadlines visit the <a href="">iGEM 2013 calendar</a>
-					</li>
-					<li>
-						Have lots of fun!
-					</li>
-				</ul> </br> </td>
-			</tr>
-		</table>
-	</body>
-</html>
+			</div><!-- /.col-lg-4 -->
+			<div class="col-lg-4">
+				<img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" style="width: 140px; height: 140px;">
+				<h2>Heading</h2>
+				<p>
+					Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+				</p>
+				<p>
+					<a class="btn btn-default" href="#" role="button">View details &raquo;</a>
+				</p>
+			</div><!-- /.col-lg-4 -->
+		</div><!-- /.row -->
+
+		<!-- START THE FEATURETTES -->
+
+		<hr class="featurette-divider">
+
+		<div class="row featurette">
+			<div class="col-md-7">
+				<h2 class="featurette-heading">First featurette heading. <span class="text-muted">It'll blow your mind.</span></h2>
+				<p class="lead">
+					Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.
+				</p>
+			</div>
+			<div class="col-md-5">
+				<img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+			</div>
+		</div>
+
+		<hr class="featurette-divider">
+
+		<div class="row featurette">
+			<div class="col-md-5">
+				<img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+			</div>
+			<div class="col-md-7">
+				<h2 class="featurette-heading">Oh yeah, it's that good. <span class="text-muted">See for yourself.</span></h2>
+				<p class="lead">
+					Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.
+				</p>
+			</div>
+		</div>
+
+		<hr class="featurette-divider">
+
+		<div class="row featurette">
+			<div class="col-md-7">
+				<h2 class="featurette-heading">And lastly, this one. <span class="text-muted">Checkmate.</span></h2>
+				<p class="lead">
+					Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.
+				</p>
+			</div>
+			<div class="col-md-5">
+				<img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+			</div>
+		</div>
+
+		<hr class="featurette-divider">
+
+		<!-- /END THE FEATURETTES -->		
+<?php
+include ('footer.php');
+?>
