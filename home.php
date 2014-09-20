03709de503e467dd3df5b1cdb61aa52a12f9a590
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2014-09-20 18:39:28 -0400

    Added canvas background

--- home.php
+++ home.php
@@ -2,7 +2,9 @@
 include ('header.php');
 ?>
 <div id="splash-wrapper">
-	<section id="splash">
+	<canvas id="canvas"></canvas>
+	
+	<section id="splash" style="position:absolute;">
 		<div class="container">
 			<div class="page-header">
 				<h1>2014 iGEM U of T: Biocontainment</h1>
@@ -11,7 +13,6 @@ include ('header.php');
 				Conventional genetic safeguards for the biocontainment of synthetic organisms often rely on various suicide mechanisms. However, these “kill-switches” suffer from a fundamental flaw since they impose selective pressure on organisms to evolve inactivation of the genetic safeguard. Activation of a kill-switch removes all individuals containing intact kill-switches from the population, leaving behind only individuals with defective kill-switches to found a new population. This project proposes an inducible plasmid-loss system to remove genetically engineered traits from a population without imposing selective pressure against the safeguard itself. The inducible plasmid-loss system is implemented by creating a plasmid containing CRISPR/Cas9, an RNA-guided nuclease system which targets and cleaves itself. A population containing this plasmid is expected to revert to wild-type upon activation. In addition to encouraging the application of evolutionary approaches to the field of synthetic biology, this project also contributes to making synthetic organisms safe for routine use outside the lab.
 			</p>
 		</div>
-
 	</section>
 	<nav>
 		<div class="container">
--- js/main.js
+++ js/main.js
@@ -1,15 +1,99 @@
-// Globals
+// RequestAnimFrame: a browser API for getting smooth animations
+window.requestAnimFrame = (function() {
+	return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame ||
+	function(callback) {
+		window.setTimeout(callback, 1000 / 60);
+	};
+})();
+
 // Make sure this is as the same value as in scss/style.scss
 var $navHeight = 60;
+// Canvas
+var canvas = document.getElementById('canvas'), ctx = canvas.getContext('2d'), cx, cy;
+var cw = canvas.width = $('html').width();
+var ch = canvas.height = $('html').height();
+// Some variables
+var d = 35;
+var x = Math.random() * (cw - d), y = Math.random() * (ch - d);
+var speedX = 5, speedY = 5;
+var directionX = 1, directionY = 1;
 
-$(document).ready(function(){
-	main();
+$(document).ready(function() {
+	resizeRelativeToWindow('#splash-wrapper', $navHeight);
+	window.addEventListener('resize', resizeCanvas, false);
+	resizeCanvas();
 });
 
-function main(){
-	resizeRelativeToWindow('#splash-wrapper', $navHeight);
+function main() {
+	clearCanvas('#000');
+	if (x > cw - d) {
+		x = cw - d;
+		randSpeed();
+		directionX *= -1;
+	} else if (x < d) {
+		x = d;
+		randSpeed();
+		directionX *= -1;
+	}
+
+	if (y < d) {
+		y = d;
+		randSpeed();
+		directionY *= -1;
+	} else if (y > ch - d) {
+		y = ch - d;
+		randSpeed();
+		directionY *= -1;
+	}
+
+	x = x + directionX * speedX;
+	y = y + directionY * speedY;
+	drawCircle(x,y,30,'#006600','#003300',false);
+
+	requestAnimFrame(function() {
+		main();
+	});
 }
 
-function resizeRelativeToWindow(element, bottomMargin){
+function resizeCanvas() {
+	cw = canvas.width = $('html').width();
+	ch = canvas.height = $('html').height();
+	cx = cw / 2;
+	cy = ch / 2;
+	
+	resizeRelativeToWindow('#splash-wrapper', $navHeight);
+	
+	main();
+}
+
+function resizeRelativeToWindow(element, bottomMargin) {
 	$(element).height($('html').height() - $navHeight);
+	
+	$('#splash').height($(element).height());
+	console.log($(element).height() + 'px');
+}
+
+function clearCanvas(col) {
+	ctx.beginPath();
+	ctx.rect(0, 0, cw, ch);
+	ctx.fillStyle = col;
+	ctx.fill();
+}
+
+function drawCircle(x, y, radius, fillCol, strCol, fill) {
+	ctx.beginPath();
+	ctx.arc(x, y, radius, 0, 2 * Math.PI, false);
+	ctx.fillStyle = fillCol;
+	if (fill) {
+		ctx.fill();
+	}
+
+	ctx.lineWidth = 5;
+	ctx.strokeStyle = strCol;
+	ctx.stroke();
+}
+
+function randSpeed() {
+	speedX = Math.random() * 5 + 5;
+	speedY = Math.random() * 5 + 5;
 }
